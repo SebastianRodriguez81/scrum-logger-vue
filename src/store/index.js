68f30755529aad8d3bde79f6dd945bebd12a649b
echigah <capurissedgard@gmail.com>
2021-05-16 18:10:45 -0300

    Se modulariza store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,13 @@
 import { createStore } from 'vuex'
+import mutations from "./mutations"
+import actions from "./actions"
 
 const store = createStore({
   state: {
     count: 0
   },
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
+  mutations: mutations,
+  actions: actions
 })
 
 export default store;
